2a859af79e5756df746464cee879781608c52e0b
AlexStrizh <strizhkoaa@gmail.com>
2024-10-18 02:58:15 +0300

    Домашняя работа по уроку "Способы вызова функции"

---
+++ module_3_2.py
@@ -0,0 +1,13 @@
+def send_email (message, recipient, sender = 'university.help@gmail.com'):
+    if not recipient.find('@') or not sender.find('@') or not recipient.endswith(('.com', '.ru', '.net'))  or not sender.endswith(('.com', '.ru', '.net')):
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес' + recipient + '.')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif  sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса ' + sender + ' на адрес ' + recipient + '.')
+    elif sender != 'university.help@gmail.com':
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ' + sender + ' на адрес ' + recipient + '.')
+
+#send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+#send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+#send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
